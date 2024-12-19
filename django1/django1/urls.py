ad71e36081f4f8b0422e794471ba0f29dcb865d2
tmkim <taeminkim13@gmail.com>
2024-12-19 14:57:38 -0500

    dfr tutorial 1 - serialization

--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -50,18 +50,24 @@
 #     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 # ]
 
-from django.urls import include, path
-from rest_framework import routers
+# from django.urls import include, path
+# from rest_framework import routers
 
-from tutorial.quickstart import views
+# from tutorial.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+
+from django.urls import path, include
+
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('snippets.urls')),
 ]
