eaec285d37273cd32cabb45ffb8a33bdafb90d71
tmkim <taeminkim13@gmail.com>
2024-12-19 22:23:11 -0500

    drf tutorial 5 - relationships and hyperlinked api

--- django1/snippets/views.py
+++ django1/snippets/views.py
@@ -1,11 +1,21 @@
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
-from django.contrib.auth.models import User
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -14,6 +24,14 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 # Using generic mixins from rest_framework
 
